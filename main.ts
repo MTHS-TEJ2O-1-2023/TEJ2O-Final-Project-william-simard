2f2116054bce61b722c1a30791b4e49f4ed77754
william <124389603+william-simard@users.noreply.github.com>
2024-01-24 22:13:20 +0000

    done

--- main.ts
+++ main.ts
@@ -1,8 +1,38 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: will simard
+ * Created on: Jan 2024
+ * This program does the Iron man thing by setting up a proximity beacon and a gesture shake to act as the repulsor shooting
 */
 
-basic.showString('Hello, World!')
+// variables
+let signal: number = 0
+const rgbRing = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
+
+// on start (from Micro:bit proximity beacon)
+radio.setGroup(11)
+radio.setTransmitPower(1)
+basic.forever(function () {
+    radio.sendString('1')
+    basic.pause(200)
+})
+
+// On shake flash rgbRing full brightness
+input.onGesture(Gesture.Shake, function () {
+    rgbRing.setBrightness(255)
+    rgbRing.showColor(neopixel.rgb(0, 40, 255))
+    basic.pause(500)
+    rgbRing.showColor(neopixel.rgb(0, 0, 0))
+})
+
+// Check recieved signal strength and if signal strength is less than -60  flash rgbRing low brightness
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    if (signal <= -60) {
+        rgbRing.setBrightness(50)
+        rgbRing.showColor(neopixel.rgb(0, 20, 255))
+    } else {
+        rgbRing.showColor(neopixel.rgb(0, 0, 0))
+    }
+})
